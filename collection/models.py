b7d8ae18f12eca28c3188a19f3fbe2b0d7791db9
dabeen-park <gamgyut777@sogang.ac.kr>
2022-08-17 23:38:24 +0900

    중간 push

--- collection/models.py
+++ collection/models.py
@@ -1,11 +1,13 @@
 from django.db import models
- 
+
 from accounts.models import Profile
-from news.models import News 
+from news.models import News
+
 # Create your models here.
 
+
 class Collection(models.Model):
-    
+
     author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     news = models.ManyToManyField(News)
     title = models.CharField(max_length=120)
@@ -13,11 +15,10 @@ class Collection(models.Model):
     likes = models.IntegerField(default=0)
     views = models.IntegerField(default=0)
     refCount = models.IntegerField(default=0)
+    tag = models.ManyToManyField(to='post.Tag')
+    editor_comment = models.TextField()
 
     def __str__(self):
         return self.title
 
-    # TODO: image, editor_comment, tag, category  필드 추가 
-
-
-
+    # TODO: image, editor_comment(반영함), tag(반영함), category  필드 추가
