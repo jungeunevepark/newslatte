5bcf69a20ef0920168934de397fd4f90e6503450
jungeunevepark <evecomcom98@naver.com>
2022-08-19 06:55:51 +0900

    static error 수정

--- newslatte/settings.py
+++ newslatte/settings.py
@@ -168,7 +168,7 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.AppDirectoriesFinder'
 ]
 
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
