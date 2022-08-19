98644895625d2cc85811f41a96b79444ade1ad08
jungeunevepark <evecomcom98@naver.com>
2022-08-19 22:28:37 +0900

    error log

--- collection/models.py
+++ collection/models.py
@@ -20,7 +20,7 @@ class Collection(models.Model):
     refCount = models.IntegerField(default=0, verbose_name="참조수")
     tag = models.ManyToManyField(to='post.Tag', null=True)
     comment = models.CharField(max_length = 120, null =True, verbose_name="코멘트")
-    image = models.URLField(null=True, blank=True),
+    image = models.URLField(null=True),
     category = models.CharField(max_length=120)
 
     def __str__(self):
