eb957847be09891d52074006d596478dd3002178
진민석 <minsjin@jinminseog-ui-MacBookAir.local>
2022-08-20 00:59:07 +0900

    글쓰기

--- newslatte/settings.py
+++ newslatte/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = 'DJANGO_SECRET'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
 
